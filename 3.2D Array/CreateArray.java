e88001619aab1b95bbd2f60bc6b10b360cc242eb
Shreyxpatil <shreyasyuvrajpatil@gmail.com>
2025-01-14 23:37:04 +0530

    Fifth Day

---
+++ 3.2D Array/CreateArray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class CreateArray {
+    
+    public static void main(String[] args) {
+        int arr[][] = new int [3][3];
+        int n =arr.length;
+        int m= arr[0].length;
+
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i<n; i++){
+            for(int j = 0;j<m; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0; i<n; i++){
+            for(int j = 0;j<m; j++){
+                System.out.print(arr[i][j]+ " " );
+            }
+            System.out.println();
+        }
+
+    }
+}
